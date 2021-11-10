2e818695590799f101dca605b8f47824e1ffb4b9
정재호 <jaho901@naver.com>
2021-11-10 20:58:43 +0900

    week7 is also done

--- week7/15685.py
+++ week7/15685.py
@@ -1,5 +1,5 @@
 import sys
-sys.stdin = open('input.txt')
+sys.stdin = open('15685.txt')
 
 '''
 x,y 시작점 / d 시작방향 / g 세대
@@ -7,6 +7,12 @@ x,y 시작점 / d 시작방향 / g 세대
 3301
 4213
 4221
+
+3301 => [0, 1세대] => [0, 1]
+4213 => [1, 3세대] => [1, 2] -> [1, 2, 3, 2] -> [1, 2, 3, 2, 3, 0, 3, 2]
+4221 => [2, 1세대] => [2, 3]
+규칙 : 뒤에서부터 1씩 더하면서 추가하기
+
 '''
 
 N = int(input())
@@ -17,15 +23,18 @@ dy = [0, -1, 0, 1]
 
 for _ in range(N):
     x, y, d, g = map(int, input().split())
+    # 처음 시작점 체크
     data[y][x] = 1
     direction = [d]
 
+    # 각각의 세대 만큼 하나의 규칙을 만들고 그 규칙을 direction에 담는다.
     for _ in range(g):
         sub = []
         for i in range(len(direction)):
             sub.append((direction[-(i+1)]+1)%4)
         direction.extend(sub)
-
+    
+    # 그 위치순으로 모두 1로 변경해주기
     for k in range(len(direction)):
         nx, ny = x+dx[direction[k]], y+dy[direction[k]]
         data[ny][nx] = 1
@@ -33,6 +42,7 @@ for _ in range(N):
 
 result = 0
 
+# 모든 위치에서 살피면서 4꼭지점에 1이 있으면 결과 result를 1씩 추가해준다.
 for i in range(102):
     for j in range(102):
         if data[i][j] == 1:
--- week7/1932.py
+++ week7/1932.py
@@ -5,15 +5,41 @@ N = int(input())
 triangle = [list(map(int, input().split())) for _ in range(N)]
 max_ans = 0
 
+# 뒤에서부터 탐구해줄 dp 어레이 생성
 dp = [[0]*i for i in range(1, N+1)]
+# 시작점 지정
 dp[0][0] = triangle[0][0]
-# 1, 2, 3, 4
+# 반복문을 통해 뒤에서부터 탐구시작
 for i in range(1, N):
     for j in range(i+1):
+        # 위에서 갈 수 있는 방법은 2가지
         left, right = j-1, j
         if left < 0:
             left = 0
         elif right == i:
             right -= 1
+        # 올라가면서 계속해서 추가해준다.
         dp[i][j] = triangle[i][j] + max(dp[i-1][left], dp[i-1][right])
 print(max(dp[N-1]))
+
+'''
+N = int(input())
+data = [list(map(int, input().split())) for _ in range(N)]
+
+
+def dfs(cur, total, i):
+    global result
+
+    if cur == N:
+        if total > result:
+            result = total
+        return
+
+    dfs(cur+1, total+data[cur][i], i)
+    dfs(cur+1, total+data[cur][i+1], i+1)
+
+
+result = 7
+dfs(1, result, 0)
+print(result)
+'''
