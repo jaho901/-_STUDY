ec6619dfdfdf0dc3ba926298600c1d8b4e601dfd
정재호 <jaho901@naver.com>
2022-01-26 20:41:41 +0900

    today is done

--- stack/1935.py
+++ stack/1935.py
@@ -1,2 +1,29 @@
 import sys
 sys.stdin = open('1935.txt')
+
+n = int(input())
+data = list(input())
+# 영문과 연산자를 구분할 수 있는 방법 => 대문자인지 아닌지
+number = []
+for _ in range(n):
+    number.append(int(input()))
+# print(number)
+stack = []
+
+for i in range(len(data)):
+    if data[i].isupper():
+        # 각각의 대문자는 숫자를 지정해둠
+        stack.append(number[ord(data[i]) - ord('A')])
+    else:
+        last_number = stack.pop()
+        first_number = stack.pop()
+        if data[i] == "+":
+            stack.append(first_number + last_number)
+        elif data[i] == "-":
+            stack.append(first_number - last_number)
+        elif data[i] == "*":
+            stack.append(first_number * last_number)
+        elif data[i] == "/":
+            stack.append(first_number / last_number)
+
+print("{:.2f}".format(stack[0]))
--- stack/2504.py
+++ stack/2504.py
@@ -1,2 +1,45 @@
 import sys
 sys.stdin = open('2504.txt')
+
+data = list(input())
+# print(data)
+stack = []
+result = 0
+sub = 1
+
+for i in range(len(data)):
+    if data[i] == "(":
+        stack.append(data[i])
+        sub *= 2
+
+    elif data[i] == "[":
+        stack.append(data[i])
+        sub *= 3
+
+    elif data[i] == ")":
+        if len(stack) == 0 or stack[-1] == "[":
+            result = 0
+            break
+        elif data[i-1] == "(":
+            result += sub
+#       그 외의 경우는 ')' or ']'가 나오는 경우
+        stack.pop()
+        sub //= 2
+
+    elif data[i] == "]":
+        if len(stack) == 0 or stack[-1] == "(":
+            result = 0
+            break
+        elif data[i-1] == "[":
+            result += sub
+        stack.pop()
+        sub //= 3
+
+if stack:
+    print(0)
+else:
+    print(result)
+
+
+
+
