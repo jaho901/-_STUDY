f9d198bc6f2a938609f83afd96c63cab73ac4675
정재호 <jaho901@naver.com>
2022-02-22 23:30:08 +0900

    problem 3, 4 are done

---
+++ Kakao_2022_Coding_Test/양궁대회.py
@@ -0,0 +1,78 @@
+
+n = 5
+info = [2,1,1,1,0,0,0,0,0,0,0]
+
+# n = 1
+# info = [1,0,0,0,0,0,0,0,0,0,0]
+
+# n = 9
+# info = [0,0,1,2,0,1,1,1,1,1,1]
+
+# n = 10
+# info = [0,0,0,0,0,0,0,0,3,4,3]
+
+
+'''
+어피치 n발 => 라이언 n발
+1. 가장 안쪽부터 10 -> 9 -> ... -> 0
+2. k점 어피치가 a발, 라이언이 b발 => 더 많이 맞춘 선수가 k점 가져감
+    -> 여기서 a = b인 경우 먼저 쏜 어피치가 k점 가져감
+3. 최종 점수가 더 높은 선수 == 우승자
+    -> 최종 점수가 같을 경우 어피치가 승리
+여기서 라이언이 어피치를 가장 큰 첨수 차이로 이기기 위한 n발의 화살 점수표!!
+만약 라이언이 우승 못할 경우 [-1] return
+
+info : 총합 = n / [10, 9, ... , 0] 의 점수를 맞춘 횟수
+라이언이 가장 큰 점수 차이로 우승할 수 있는 방법이 여러 가지 일 경우,
+가장 낮은 점수를 더 많이 맞힌 경우를 return
+'''
+
+result = []
+total = 0
+def solution(n, info):
+    global result
+
+    answer = [0] * 11
+
+    def recur(cur, index):
+        global result, total
+        if cur == n:
+            peach, lion = 0, 0
+            for i in range(11):
+                if info[i] >= answer[i] and info[i] != 0:
+                    peach += (10-i)
+                elif info[i] < answer[i] and answer[i] != 0:
+                    lion += (10-i)
+            if lion > peach:
+                diff = lion - peach
+                if diff > total:
+                    result = [answer[:]]
+                    total = diff
+                elif diff == total:
+                    result.append(answer[:])
+            return
+
+        for i in range(index, 11):
+            answer[i] += 1
+            recur(cur+1, i)
+            answer[i] -= 1
+
+    recur(0, 0)
+
+    if result==[]:
+        result = [-1]
+    elif len(result) == 1:
+        result = result[0]
+    else:
+        for i in range(10, -1, -1):
+            sub = []
+            for j in range(len(result)):
+                sub.append(result[j][i])
+            if sub.count(max(sub)) != len(result):
+                result = result[sub.index(max(sub))]
+                break
+
+    return result
+
+
+print(solution(n, info))
---
+++ Kakao_2022_Coding_Test/주차_요금_계산.py
@@ -0,0 +1,60 @@
+# fees = [180, 5000, 10, 600]
+# records = ["05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", "07:59 5961 OUT", "07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"]
+
+# fees = [120, 0, 60, 591]
+# records = ["16:00 3961 IN","16:00 0202 IN","18:00 3961 OUT","18:00 0202 OUT","23:58 3961 IN"]
+
+fees = [1, 461, 1, 10]
+records = ["00:00 1234 IN"]
+
+# fees = [기본 시간(분), 기본 요금(원), 단위 시간(분), 단위 요금(원)]
+# records 에서 (OUT으로 된거 시각 - IN으로 된거 시각) if 180이상 =>
+
+def solution(fees, records):
+    result = dict()
+    sub = []
+    # 차별 시간 총합 저장본
+    answer = []
+
+    for i in range(len(records)):
+        timein, number, type = records[i].split()
+        if number not in result:
+            result[number] = 0
+    result = sorted(result.items())
+    result = dict(result)
+
+    for i in range(len(records)):
+        timein, number, type = records[i].split()
+        if type == 'IN':
+            sub.append([timein, number])
+
+        elif type == 'OUT':
+            for j in range(len(sub)):
+                ti, num = sub[j][0], sub[j][1]
+                if num == number:
+                    numberout = num
+                    timeout = ti
+                    sub.pop(j)
+                    break
+            time = (int(timein[:2])*60+int(timein[3:]) - (int(timeout[:2])*60+int(timeout[3:])))
+            result[numberout] += time
+
+    for x, y in sub:
+        result[y] += ((23*60+59)-(int(x[:2])*60+int(x[3:])))
+
+    basic_time, basic_fee, per_time, per_fee = fees[0], fees[1], fees[2], fees[3]
+
+    for i, key in enumerate(result):
+        if result[key] <= basic_time:
+            answer.append(basic_fee)
+        else:
+            if (result[key] - basic_time)%per_time==0:
+                answer.append(basic_fee + ((result[key] - basic_time)//per_time)*per_fee)
+            else:
+                answer.append(basic_fee + (((result[key] - basic_time) // per_time)+1) * per_fee)
+
+    return answer
+
+
+
+print(solution(fees, records))
